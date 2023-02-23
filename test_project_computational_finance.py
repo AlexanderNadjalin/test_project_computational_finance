f4b1feecf025fccbcd06434a4f2d289ebd929fd8
Alex <alexander@nadjalin.com>
2023-02-23 17:10:48 +0100

    Added GBM text and code for basic MC.

--- test_project_computational_finance.py
+++ test_project_computational_finance.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 
 class RnDisc:
@@ -41,8 +42,41 @@ class MarketAndOption:
         self.sigma = sigma
         self.r = r
         self.option_type = option_type
-        self.expiry = expiry
+        self.expiry = expiry * 365
         self.k = k
         self.num_steps = num_steps
         self.dt = expiry / num_steps
         self.mc_paths = None
+
+        # Run the Monte-Carlo simulation.
+        self.mc_sim()
+
+    def mc_sim(self):
+        # Pre-simulation setup.
+        rand_s = np.random.standard_normal(self.num_steps)
+        self.mc_paths = np.zeros_like(rand_s)
+        self.mc_paths[0] = self.s0
+
+        # The Euler discretization at time t.
+        for t in range(1, self.num_steps):
+            self.mc_paths[t] = self.mc_paths[t - 1] * \
+                               math.exp((self.r - self.sigma * self.sigma / 2) * self.dt +
+                                        self.sigma * math.sqrt(self.dt) * rand_s[t])
+
+
+if __name__ == '__main__':
+    s0 = 100
+    sigma = 0.2
+    r = 0.05
+    expiry = 1
+    k = 100
+    num_steps = 1000
+
+    d = MarketAndOption(s0=s0,
+                        sigma=sigma,
+                        r=r,
+                        option_type='d',
+                        expiry=expiry,
+                        k=k,
+                        num_steps=num_steps)
+    print(d)
